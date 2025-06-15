63b6821110a0b1329f17c1e27864277f5f035527
Nikius-PC\Nikius <mikhail.dudek@gmail.com>
2025-06-15 14:42:27 +0300

    fixes

--- inventory.cs
+++ inventory.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 public class Inventory
 {
@@ -14,12 +15,17 @@ public class Inventory
             Add(item);
     }
 
-    public List<Item> Items => _items;
+    public IReadOnlyList<Item> Items => _items;
+
+    public bool HasFreeSpace() => _items.Count < _maxSize;
 
     public void Add(Item item)
     {
-        if (_items.Count >= _maxSize)
+        if (HasFreeSpace() == false)
+        {
+            Debug.Log("Inventory is full! Item wasn't added.");
             return;
+        }
 
         _items.Add(item);
     }
@@ -27,6 +33,9 @@ public class Inventory
     public List<Item> ExtractItemsBy(int id, int count)
     {
         List<Item> extractedItems = _items.Where(item => item.ID == id).Take(count).ToList();
+        
+        if (extractedItems.Count < count)
+            Debug.Log($"There are only {extractedItems.Count} items with id {id} in inventory. Requested {count} items.");
         
         foreach (Item item in extractedItems)
             _items.Remove(item);
@@ -37,10 +46,10 @@ public class Inventory
 
 public class Item
 {
-    public int ID;
-
     public Item(int id)
     {
         ID = id;
     }
+
+    public int ID { get; private set; }
 }
